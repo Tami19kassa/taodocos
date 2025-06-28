7256bdf1bc19de2c7bf32e6279e538dc06c21e8f
Tami19kassa <123267708+Tami19kassa@users.noreply.github.com>
2025-06-29 01:58:16 +0300

    Update route.js

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -3,7 +3,7 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEYs);
 
 export async function POST(request) {
   try {
